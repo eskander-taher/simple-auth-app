629c015ad1bcb28f1596d3580056081531f53203
eskander-taher <eskander.alshaibany@gmail.com>
2024-01-18 20:24:38 +0500

    add prisma

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,21 @@ const app = express();
 
 import cors from "cors";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 app.use(cors());
 app.use(express.json());
 
-app.post("/", (req, res) => {
-	res.json(req.body);
+app.post("/", async (req, res) => {
+	try {
+		const newUser = await prisma.user.create({
+			data: req.body,
+		});
+		res.json(newUser);
+	} catch (error) {
+		res.json(error)
+	}
 });
 
 app.listen(3000, () => console.log("listening on port 3000"));
